62c4d8148a1ba8605999ba72c8242c3da516535a
hasnaeMady <102815173+hasnaeMady@users.noreply.github.com>
2022-05-29 13:01:04 +0100

    Delete frontend/app directory

--- frontend/app/Models/customer/customer.model.ts
+++
@@ -1,9 +0,0 @@
-export class Customer {
-     Name: string;
-     Phonenumber: number;
-     home: any;
-     Work: any;
-     Other: any;
-}
-
-//la table customer n'existe pas
--- frontend/app/Pipe/platsearchpipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlatsearchpipePipe } from './platsearchpipe.pipe';
-
-describe('PlatsearchpipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlatsearchpipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
