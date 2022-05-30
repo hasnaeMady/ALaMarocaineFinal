555042ec60bbd3592188838a212964b4694213f1
nouhaila12-iu <81149335+nouhaila12-iu@users.noreply.github.com>
2022-05-30 17:18:51 +0100

    Update UserController.java

--- backend/src/main/java/com/bridgelabz/alamarocaine/controller/UserController.java
+++ backend/src/main/java/com/bridgelabz/alamarocaine/controller/UserController.java
@@ -1,5 +1,4 @@
-package com.bridgelabz.alamarocaine.controller;
-
+package com.bridgelabz.bookstore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,23 +12,26 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bridgelabz.alamarocaine.dto.UserDto;
-import com.bridgelabz.alamarocaine.entity.Users;
-import com.bridgelabz.alamarocaine.exception.UserException;
-import com.bridgelabz.alamarocaine.request.LoginInformation;
-import com.bridgelabz.alamarocaine.request.PasswordReset;
-import com.bridgelabz.alamarocaine.request.PasswordUpdate;
-import com.bridgelabz.alamarocaine.response.Response;
-import com.bridgelabz.alamarocaine.response.UsersDetailRes;
-import com.bridgelabz.alamarocaine.service.UserServices;
-import com.bridgelabz.alamarocaine.util.JwtGenerator;
+import com.bridgelabz.bookstore.dto.EditBookDto;
+import com.bridgelabz.bookstore.dto.EditUserDto;
+import com.bridgelabz.bookstore.dto.UserDto;
+import com.bridgelabz.bookstore.entity.Book;
+import com.bridgelabz.bookstore.entity.Users;
+import com.bridgelabz.bookstore.exception.UserException;
+import com.bridgelabz.bookstore.request.LoginInformation;
+import com.bridgelabz.bookstore.request.PasswordReset;
+import com.bridgelabz.bookstore.request.PasswordUpdate;
+import com.bridgelabz.bookstore.response.*;
+import com.bridgelabz.bookstore.service.UserServices;
+import com.bridgelabz.bookstore.util.JwtGenerator;
 
 @RestController
 @CrossOrigin
 public class UserController {
-
+	
 	@Autowired
 	private UserServices service;
+	
 
 	@Autowired
 	private JwtGenerator generate;
@@ -44,26 +46,27 @@ public class UserController {
 					.body(new Response("registration successfull", 200, information));
 		} else {
 			return ResponseEntity.status(HttpStatus.ALREADY_REPORTED)
-					.body(new Response("Utilisateur existe déjà!!", 400, information));
+					.body(new Response("User Already Exist", 400, information));
 		}
 	}
-
+	
+	
 	@PostMapping("/user/login")
 	public ResponseEntity<UsersDetailRes> login(@RequestBody LoginInformation information) {
-
+		
 		Users users = service.login(information);
-		if (users != null) {
-			String token = generate.jwtToken(users.getUserId());
+		if (users!=null) {
+			String token= generate.jwtToken(users.getUserId());
 			return ResponseEntity.status(HttpStatus.ACCEPTED).header("login successfull", information.getEmail())
 					.body(new UsersDetailRes(token, 200, users));
-		} else {
+		}
+		else {
 			throw new UserException(" Invalide credentials");
 		}
 	}
-
+	
 	/**
 	 * This is for the user verify.......
-	 * 
 	 * @param token
 	 * @return response as success and fail
 	 * @throws Exception
@@ -72,47 +75,64 @@ public class UserController {
 	public ResponseEntity<Response> userVerification(@PathVariable("token") String token) throws Exception {
 		System.out.println("token for verification" + token);
 		boolean update = service.verify(token);
-		if (update) {
+		if (update)
+		{
 			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("verified", 200));
-		} else {
+		} 
+		else 
+		{	
 			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("not verified", 400));
 		}
 	}
-
+	
 	/**
 	 * This is used for the get one user based on there token
-	 * 
 	 * @param token
 	 * @return response
 	 */
 	@PostMapping("user/forgotpassword")
 	public ResponseEntity<Response> forgogPassword(@RequestBody PasswordReset passwordReset) {
-
+	
 		boolean result = service.isUserExist(passwordReset.getEmail());
 		if (result) {
 			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("Check your email: Email sent", 200));
 		} else {
-			return ResponseEntity.status(HttpStatus.ACCEPTED)
-					.body(new Response("user does not exist with given email id", 400));
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("user does not exist with given email id", 400));
 		}
 	}
-
+	
 	@PutMapping("user/update/{token}")
 	public ResponseEntity<Response> update(@PathVariable("token") String token, @RequestBody PasswordUpdate update) {
-		System.out.println("inside controller  " + token);
+		System.out.println("inside controller  " +token);
 		boolean result = service.update(update, token);
 		if (result) {
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("", 200));
+			return ResponseEntity.status(HttpStatus.ACCEPTED)
+					.body(new Response("password updated successfully", 200));
 		} else {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response("password doesn't match", 401));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new Response("password doesn't match", 401));
 		}
 
 	}
-
+	
 	@GetMapping("user/getOneUser")
-	public ResponseEntity<Response> getOneUsers(@RequestHeader("token") String token) {
-		Users user = service.getSingleUser(token);
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("user is", 200, user));
+	public ResponseEntity<Response> getOneUser(@RequestHeader("token") String token){
+	Users user=service.getOneUser(token);
+		return ResponseEntity.status(HttpStatus.ACCEPTED)
+				.body(new Response("user is", 200, user));
 	}
 
-}
+	@GetMapping(value = "users/getuser/{userId}")
+	public ResponseEntity<UserResponse> getUserbyId(@PathVariable("userId") Long userId) {
+		Users info = service.getUserbyId(userId);
+		return ResponseEntity.status(HttpStatus.OK).body(new UserResponse("The user is", info));
+	}
+
+	@PutMapping("users/{userId}")
+	public ResponseEntity<UserResponse> editUser(@PathVariable("userId") long userId,@RequestBody EditUserDto information,@RequestHeader("token") String token){
+		boolean res =service.editUser(userId,information,token);
+		if(res)
+			return ResponseEntity.status(HttpStatus.CREATED).body(new UserResponse(200, "L'utilisateur est mis à jour avec succès"));
+		return null;
+	
+}}
