064ee66374bb7990fc9403c9631607d2ae259c2e
nouhaila12-iu <81149335+nouhaila12-iu@users.noreply.github.com>
2022-06-05 11:48:25 +0100

    Create EditUserDto.java

---
+++ backend/src/main/java/com/bridgelabz/alamarocaine/dto/EditUserDto.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.bookstore.dto;
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+@Data
+@Component
+public class EditUserDto {
+		private String name;
+		private Long mobileNumber;
+		private String email;
+		private String password;
+		private LocalDateTime updatedAt;
+		
+}
