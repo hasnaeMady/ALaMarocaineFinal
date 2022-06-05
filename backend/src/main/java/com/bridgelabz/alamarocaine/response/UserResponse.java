9a75ee38dfa10346ecf190e0234b42bb7bcce9c6
nouhaila12-iu <81149335+nouhaila12-iu@users.noreply.github.com>
2022-06-05 11:58:23 +0100

    Update UserResponse.java

--- backend/src/main/java/com/bridgelabz/alamarocaine/response/UserResponse.java
+++ backend/src/main/java/com/bridgelabz/alamarocaine/response/UserResponse.java
@@ -1,35 +1,18 @@
-package com.bridgelabz.alamarocaine.response;
+package com.bridgelabz.bookstore.response;
+
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 
+import com.bridgelabz.bookstore.entity.Book;
+import com.bridgelabz.bookstore.entity.Users;
+
 public class UserResponse {
+	Users user;
 
-	private String Message;
-	private HttpStatus status;
 	private Object obj;
-
-	public UserResponse(String message, Object obj, HttpStatus status) {
-		super();
-		this.status = status;
-		Message = message;
-		this.obj = obj;
-	}
-
-	public String getMessage() {
-		return Message;
-	}
-
-	public void setMessage(String message) {
-		Message = message;
-	}
-
-	public HttpStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(HttpStatus status) {
-		this.status = status;
-	}
+	double rate;
+	
 
 	public Object getObj() {
 		return obj;
@@ -37,6 +20,59 @@ public class UserResponse {
 
 	public void setObj(Object obj) {
 		this.obj = obj;
+	}
+
+	int statusCode;
+	String response;
+	List<Users> userList;
+
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+	public List<Users> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<Users> userList) {
+		this.userList = userList;
+	}
+
+	public UserResponse() {
+
+	}
+
+	public UserResponse(String response, Object obj) {
+		super();
+		this.obj = obj;
+
+		this.response = response;
+	}
+
+	public UserResponse(int statusCode, String response) {
+		super();
+		this.statusCode = statusCode;
+		this.response = response;
 	}
 
 }
